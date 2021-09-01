7499e7806d947e324b3c6ac6f0fab67f600d8541
ChikaLong <longb.0818@gmail.com>
2021-09-01 13:59:33 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9d7397af1de8ef8ad4c83b7b0095a89b11e2efdd9ae5a4b425bc288596f9c11ba5a395952bc9361e8f6149e7fcfa4b5f768a778d84698bc3750d1d8b2c112185'

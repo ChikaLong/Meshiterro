5359028a192207c0a36db652478daf8d06c0e4fe
ChikaLong <longb.0818@gmail.com>
2021-09-06 03:44:28 +0000

    [Add]フラッシュメッセージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     post_images_path
   end
+  
+  def after_sign_up_path_for(resource)
+    post_images_path
+  end
+
 
   protected
 
